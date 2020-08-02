e9211a5e36a582c2af114c1603d5633dd326d97d
Jodie <mail@jodie.website>
2020-08-02 05:28:43 -0400

    refactor

--- index.js
+++ index.js
@@ -1,65 +1,86 @@
 const plugin = require("tailwindcss/plugin");
 
+// list of unset values for ie support (in progress)
+const unsetValues = {
+	animation: "none",
+	animationDelay: "0s",
+	animationDirection: "normal",
+	animationDuration: "0s",
+	animationFillMode: "none",
+	animationIterationCount: "1",
+	animationName: "none",
+	animationPlayState: "running",
+	animationTimingFunction: "ease",
+	backfaceVisibility: "visible",
+	background: "none",
+	backgroundAttachment: "scroll",
+	backgroundClip: "border-box",
+	backgroundColor: "transparent",
+	backgroundImage: "none",
+	backgroundOrigin: "padding-box",
+	backgroundPosition: "0 0",
+	backgroundPositionX: "0",
+	backgroundPositionY: "0",
+	backgroundRepeat: "repeat",
+	backgroundSize: "auto auto",
+	border: "medium none",
+	borderStyle: "none",
+	borderWidth: "medium",
+	borderColor: "currentColor",
+
+	color: "inherit",
+	font: "inherit",
+	height: "auto",
+	margin: "0",
+	textDecoration: "none",
+};
+
+const cloneObject = (object) => Object.assign({}, object);
+
 module.exports = plugin(
 	({ addBase, theme }) => {
 		const options = theme("extinguish", {});
 
-		var unset = options.legacy
-			? {
-					margin: "0",
-					color: "inherit",
+		const polyfillUnset = options.legacy
+			? (object) => {
+					const newObject = {};
+					Object.entries(object).forEach(([key, value]) => {
+						newObject[key] = value === "unset" ? unsetValues[key] : value;
+					});
+					return newObject;
 			  }
-			: {
-					margin: "unset",
-					color: "unset",
-			  };
+			: cloneObject;
 
-		var reset = {
+		const unset = polyfillUnset({
+			margin: "unset",
+			color: "unset", // affects: a, mark, input, textarea, keygen, select, button
+			...(options.font && { font: "unset" }),
+		});
+
+		const reset = {
 			//boxSizing: "inherit", // will be enabled in v1
 			border: "solid 0",
 			minWidth: "0", // prevents odd behavior with flexbox
 			minHeight: "0", // same
+			...(options.layout && { contain: "layout" }),
+			...(options.borderColor && { borderColor: "inherit" }),
 		};
-
-		if (options.font) {
-			Object.assign(unset, {
-				font: options.legacy ? "inherit" : "unset" + " /* font option */",
-			});
-		}
 
 		// will be removed in v1
 		Object.assign(
 			unset,
-			options.legacy
-				? {
-						background: "none", // mostly for inputs (deprecated)
-						textDecoration: "none", // mostly for links (deprecated)
-				  }
-				: {
-						background: "unset", // mostly for inputs (deprecated)
-						textDecoration: "unset", // mostly for links (deprecated)
-				  }
+			polyfillUnset({
+				background: "unset", // mostly for inputs (deprecated)
+				textDecoration: "unset", // mostly for links (deprecated)
+			})
 		);
 
 		// will be removed in v1
 		if (options.boxSizing) {
-			Object.assign(reset, { boxSizing: "inherit /* boxSizing option */" });
+			Object.assign(reset, {
+				boxSizing: "inherit",
+			});
 		}
-
-		if (options.layout) {
-			Object.assign(reset, { contain: "layout /* layout option */" });
-		}
-
-		if (options.borderColor) {
-			Object.assign(reset, { borderColor: "inherit /* borderColor option */" });
-		}
-
-		addBase({
-			// will be enabled in v1
-			//"a": { textDecoration: options.legacy ? "none" : "unset" },
-			// will be enabled once <dialog> is supported in firefox and safari
-			//"dialog": { margin: "auto" },
-		});
 
 		if (options.pseudoElements) {
 			addBase({
@@ -75,6 +96,23 @@ module.exports = plugin(
 			});
 		}
 
+		addBase({
+			// will be enabled in v1
+			//"a": polyfillUnset({ textDecoration: "unset" }),
+			// will be enabled once <dialog> is supported in firefox and safari
+			//"dialog": { margin: "auto" },
+			// will be enabled after purgecss testing
+			//"pre": { tabSize: "4" },
+		});
+
+		if (options.legacy && !options.font) {
+			addBase({
+				pre: {
+					fontSize: "inherit",
+				},
+			});
+		}
+
 		if (options.placeholders) {
 			addBase({
 				"/* placeholder option */\n*::placeholder": {
@@ -85,9 +123,9 @@ module.exports = plugin(
 
 		if (options.images) {
 			addBase({
-				"/* images option */\nimg, video": {
-					height: options.legacy ? "auto" : "unset",
-				},
+				"/* images option */\nimg, video": polyfillUnset({
+					height: "unset",
+				}),
 			});
 		}
 
@@ -104,7 +142,7 @@ module.exports = plugin(
 		theme: {
 			extinguish: {
 				legacy: true,
-				boxSizing: false, // will be true in v1
+				boxSizing: false, // will be removed in v1
 				layout: false, // add paint option in the future?
 				borderColor: true, // will be false in v1
 				font: true,
